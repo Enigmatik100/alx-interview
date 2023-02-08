253f6ecc3bc50817507953cd7313230624aa235f
enigmatik100 <m.p.gangbadja@gmail.com>
2023-02-08 11:09:53 +0100

    start war api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,16 @@
+const request = require('request');
+const id = parseInt(process.argv[2]);
+const URL = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+request(URL, { json: true }, async (err, res, body) => {
+  if (err) console.log(err);
+  const characters = body.characters;
+  for (const charUrl of characters) {
+    await new Promise((resolve, reject, body) => {
+      request(charUrl, { json: true }, (error, response, responseBody) => {
+        if (error) reject(err);
+        resolve(responseBody);
+        console.log(responseBody.name);
+      });
+    });
+  }
+});
